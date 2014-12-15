34ce09bb6590ae3ba7fe1bb01c6556aa4c7ea84f
Julian Ceipek <julian.ceipek@gmail.com>
2014-12-15 15:30:36 -0800

    Switched most AST stuff to mori for data representation

--- lang/evaluator.js
+++ lang/evaluator.js
@@ -1,4 +1,5 @@
 import S from './symbolTypes';
+var M = require("mori"); // Couldn't figure out how to convert to ECMAScript6
 
 var makeAssignment = function (sym, r) {
   return { status: 'ASSIGNMENT', symbol: sym, value: r };
@@ -13,88 +14,88 @@ var makeError = function (e, vals, pos) {
 }
 
 var evaluateAdd = function (a, b, pos, scope) {
-  if (a.type === S.T_INT && b.type === S.T_INT) {
-    return makeResult(S.makeInt(a.value + b.value, pos));
-  } else if (a.type === S.T_FLOAT && b.type === S.T_FLOAT) {
-    return makeResult(S.makeFloat(a.value + b.value, pos));
+  if (M.get(a,'type') === S.T_INT && M.get(b,'type') === S.T_INT) {
+    return makeResult(S.makeInt(M.get(a, 'value') + M.get(b,'value'), pos));
+  } else if (M.get(a,'type') === S.T_FLOAT && M.get(b,'type') === S.T_FLOAT) {
+    return makeResult(S.makeFloat(M.get(a, 'value') + M.get(b,'value'), pos));
   } else {
-    return makeError("I don't understand addition on "+a.type.toString()+" and "+b.type.toString()
+    return makeError("I don't understand addition on "+M.get(a,'type').toString()+" and "+M.get(b,'type').toString()
                     , [a, b], pos);
   }
 };
 
 var evaluateSub = function (a, b, pos, scope) {
-  if (a.type === S.T_INT && b.type === S.T_INT) {
-    return makeResult(S.makeInt(a.value - b.value, pos));
-  } else if (a.type === S.T_FLOAT && b.type === S.T_FLOAT) {
-    return makeResult(S.makeFloat(a.value - b.value, pos));
+  if (M.get(a,'type') === S.T_INT && M.get(b,'type') === S.T_INT) {
+    return makeResult(S.makeInt(M.get(a, 'value') - M.get(b,'value'), pos));
+  } else if (M.get(a,'type') === S.T_FLOAT && M.get(b,'type') === S.T_FLOAT) {
+    return makeResult(S.makeFloat(M.get(a, 'value') - M.get(b,'value'), pos));
   } else {
-    return makeError("I don't understand subtraction on "+a.type.toString()+" and "+b.type.toString()
+    return makeError("I don't understand subtraction on "+M.get(a,'type').toString()+" and "+M.get(b,'type').toString()
                     , [a, b], pos);
   }
 };
 
 var evaluateMul = function (a, b, pos, scope) {
-  if (a.type === S.T_INT && b.type === S.T_INT) {
-    return makeResult(S.makeInt(a.value * b.value, pos));
-  } else if (a.type === S.T_FLOAT && b.type === S.T_FLOAT) {
-    return makeResult(S.makeFloat(a.value * b.value, pos));
+  if (M.get(a,'type') === S.T_INT && M.get(b,'type') === S.T_INT) {
+    return makeResult(S.makeInt(M.get(a, 'value') * M.get(b,'value'), pos));
+  } else if (M.get(a,'type') === S.T_FLOAT && M.get(b,'type') === S.T_FLOAT) {
+    return makeResult(S.makeFloat(M.get(a, 'value') * M.get(b,'value'), pos));
   } else {
-    return makeError("I don't understand multiplication on "+a.type.toString()+" and "+b.type.toString()
+    return makeError("I don't understand multiplication on "+M.get(a,'type').toString()+" and "+M.get(b,'type').toString()
                     , [a, b], pos);
   }
 };
 
 var evaluateDiv = function (a, b, pos, scope) {
-  if (a.type === S.T_INT && b.type === S.T_INT) {
-    var div = a.value / b.value;
+  if (M.get(a,'type') === S.T_INT && M.get(b,'type') === S.T_INT) {
+    var div = M.get(a, 'value') / M.get(b,'value');
     var f = Math.trunc(div); // floor doesn't work correctly for negative numbers
     var res = S.makeInt(f, pos);
     res._couldLosePrecision = true;
     res._didLosePrecision = (f !== div);
     return makeResult(res);
-  } else if (a.type === S.T_FLOAT && b.type === S.T_FLOAT) {
-    return makeResult(S.makeFloat(a.value / b.value, pos));
+  } else if (M.get(a,'type') === S.T_FLOAT && M.get(b,'type') === S.T_FLOAT) {
+    return makeResult(S.makeFloat(M.get(a, 'value') / M.get(b,'value'), pos));
   } else {
-    return makeError("I don't understand division on "+a.type.toString()+" and "+b.type.toString()
+    return makeError("I don't understand division on "+M.get(a,'type').toString()+" and "+M.get(b,'type').toString()
                     , [a, b], pos);
   }
 };
 
 var evaluateEq = function (a, b, pos, scope) {
-  if (a.type !== b.type) {
+  if (M.get(a,'type') !== M.get(b,'type')) {
     return makeResult(S.makeBool(false, pos));
-  } else if (a.type === S.T_INT || // Don't need to check b because we know the types match
-             a.type === S.T_FLOAT ||
-             a.type === S.T_BOOL) {
-    return makeResult(S.makeBool(a.value == b.value, pos));
+  } else if (M.get(a,'type') === S.T_INT || // Don't need to check b because we know the types match
+             M.get(a,'type') === S.T_FLOAT ||
+             M.get(a,'type') === S.T_BOOL) {
+    return makeResult(S.makeBool(M.get(a, 'value') == M.get(b,'value'), pos));
   } else {
-    return makeError("I don't understand equivalence checks on "+a.type.toString()+" and "+b.type.toString()
+    return makeError("I don't understand equivalence checks on "+M.get(a,'type').toString()+" and "+M.get(b,'type').toString()
                     , [a, b], pos);
   }
 };
 
 var evaluateNeg = function (node, pos, scope) {
-  if (node.type === S.T_INT) {
-    return makeResult(S.makeInt(-node.value, pos));
-  } else if (node.type === S.T_FLOAT) {
-    return makeResult(S.makeFloat(-node.value, pos));
+  if (M.get(node,'type') === S.T_INT) {
+    return makeResult(S.makeInt(-M.get(node,'value'), pos));
+  } else if (M.get(node,'type') === S.T_FLOAT) {
+    return makeResult(S.makeFloat(-M.get(node,'value'), pos));
   } else {
-    return makeError("I don't understand negation on "+node.type.toString()
+    return makeError("I don't understand negation on "+M.get(node,'type').toString()
                     , [node], pos);
   }
 };
 
 var evaluateUnaryOperator = function (node, scope) {
-  var res = node.value;
+  var res = M.get(node,'value');
   if (!S.isTerminal(res)) {
-    res = evaluateASTTree(node.value, scope);
+    res = evaluateASTTree(res, scope);
     if (res.status === 'ERROR') {
       return res;
     }
   }
-  if (node.type === S.E_NEG) {
-    res = evaluateNeg(res.res, node.pos, scope);
+  if (M.get(node,'type') === S.E_NEG) {
+    res = evaluateNeg(res.res, M.get(node,'pos'), scope);
   }
   node._cachedResult = res;
   if (res.status === 'ERROR') {
@@ -104,17 +105,17 @@ var evaluateUnaryOperator = function (node, scope) {
 }
 
 var evaluateBinaryOperator = function (node, scope) {
-  var resA = node.valueA;
-  var resB = node.valueB;
+  var resA = M.get(node,'valueA');
+  var resB = M.get(node,'valueB');
   if (!S.isTerminal(resA)) {
-    resA = evaluateASTTree(node.valueA, scope);
+    resA = evaluateASTTree(resA, scope);
     if (resA.status === 'ERROR') {
       return resA;
     }
     resA = resA.res;
   }
   if (!S.isTerminal(resB)) {
-    resB = evaluateASTTree(node.valueB, scope);
+    resB = evaluateASTTree(resB, scope);
     if (resB.status === 'ERROR') {
       return resB;
     }
@@ -122,16 +123,17 @@ var evaluateBinaryOperator = function (node, scope) {
   }
 
   var res = null;
-  if (node.type === S.E_ADD) {
-    res = evaluateAdd(resA, resB, node.pos, scope);
-  } else if (node.type === S.E_SUB) {
-    res = evaluateSub(resA, resB, node.pos, scope);
-  } else if (node.type === S.E_MUL) {
-    res = evaluateMul(resA, resB, node.pos, scope);
-  } else if (node.type === S.E_DIV) {
-    res = evaluateDiv(resA, resB, node.pos, scope);
-  } else if (node.type === S.E_EQ) {
-    res = evaluateEq(resA, resB, node.pos, scope);
+  var pos = M.get(node, 'pos');
+  if (M.get(node,'type') === S.E_ADD) {
+    res = evaluateAdd(resA, resB, pos, scope);
+  } else if (M.get(node,'type') === S.E_SUB) {
+    res = evaluateSub(resA, resB, pos, scope);
+  } else if (M.get(node,'type') === S.E_MUL) {
+    res = evaluateMul(resA, resB, pos, scope);
+  } else if (M.get(node,'type') === S.E_DIV) {
+    res = evaluateDiv(resA, resB, pos, scope);
+  } else if (M.get(node,'type') === S.E_EQ) {
+    res = evaluateEq(resA, resB, pos, scope);
   }
   node._cachedResult = res;
   if (res.status === 'ERROR') {
@@ -147,28 +149,31 @@ var evaluateTerminal = function (node, scope) {
 var evaluateSymbol = function (node, scope) {
   // TODO: Think about optimizing this if it becomes a bottleneck;
   // it has to walk the scope 2x for the sake of avoiding try/catch
-  if (scope.scopeOfSymbol(node.name) === null) {
-    var e = makeError("I can't find "+node.name.toString()+" in my scope or parent scopes :("
-                    , [node], node.pos);
+  var name = M.get(node,'name');
+  if (scope.scopeOfSymbol(name) === null) {
+    var e = makeError("I can't find "+name.toString()+" in my scope or parent scopes :("
+                    , [node], M.get(node,'pos'));
     node._cachedResult = e;
     return e;
   }
-  var s = scope.lookupSymbolValue(node.name);
-  s.pos = node.pos; // pos actually doesn't make much sense for values
-                    // mapped to symbols in the scope contents.
-                    // However, when we do a symbol lookup, we can set it
-                    // to the pos of the symbol we replaced.
+  var s = scope.lookupSymbolValue(name);
+  s = M.assoc(s, 'pos', M.get(node,'pos'));
+  // pos actually doesn't make much sense for values
+  // mapped to symbols in the scope contents.
+  // However, when we do a symbol lookup, we can set it
+  // to the pos of the symbol we replaced.
+
   s = makeResult(s);
   node._cachedResult = s;
   return s;
 };
 
 var evaluateAssign = function (node, scope) {
-  var res = evaluateASTTree(node.expr, scope);
+  var res = evaluateASTTree(M.get(node,'expr'), scope);
   if (res.status === 'ERROR') {
     return res;
   }
-  return makeAssignment(node.symbol, res.res);
+  return makeAssignment(M.get(node,'symbol'), res.res);
 };
 
 // NOTE: As this is architected, this MUST NOT modify the scope. EVER.
@@ -180,19 +185,19 @@ export default function evaluateASTTree (node, scope) {
   if (!node) {
     return;
   }
-  if (node.type === S.S_ASSIGN) {
+  if (M.get(node,'type') === S.S_ASSIGN) {
     return evaluateAssign(node, scope);
-  } else if (S.isUnaryOperator(node.type)) {
+  } else if (S.isUnaryOperator(M.get(node,'type'))) {
     return evaluateUnaryOperator(node, scope);
-  } else if (S.isBinaryOperator(node.type)) {
+  } else if (S.isBinaryOperator(M.get(node,'type'))) {
     return evaluateBinaryOperator(node, scope);
-  } else if (node.type === S.T_SYM) { // Needs to be above isTerminal because we don't want to evaluate symbols before lookup
+  } else if (M.get(node,'type') === S.T_SYM) { // Needs to be above isTerminal because we don't want to evaluate symbols before lookup
     return evaluateSymbol(node, scope);
-  } else if (S.isTerminal(node.type)) {
+  } else if (S.isTerminal(M.get(node,'type'))) {
     return evaluateTerminal(node, scope);
   } else {
-    return makeError("I don't understand "+node.type.toString()
-                    , [node], node.pos);
+    return makeError("I don't understand "+M.get(node,'type').toString()
+                    , [node], M.get(node,'pos'));
     // S.E_CALL
   }
 };
